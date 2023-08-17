7c9b8b78121ecbbf3d840d194381f338b003d49d
kendagordavid <davidkendgor7@gmail.com>
2023-08-17 19:27:18 +0300

    updated  set isdisjoint()

---
+++ set_isdisjoint.py
@@ -0,0 +1,10 @@
+""" Python set isdisjoint() function check whether the two sets are disjoint or not, 
+if it is disjoint then it returns True otherwise it will return False.
+ Two sets are said to be disjoint when their intersection is null.  """
+ #in list
+set_a = {"green","yellow","red","pink","orange","blue","black"}
+set_b = {"green","yellow","red","pink","orange","blue","black"}
+print(set_a.isdisjoint(set_b))
+
+""" Expected output should be: false.This means that the two sets a and b, are joint and not disjoint """
+
