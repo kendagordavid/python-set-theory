66e9ebca7b6c6a4801336fba4b36ff89e5dd8025
kendagordavid <davidkendgor7@gmail.com>
2023-08-18 12:16:15 +0300

    updated set_discard()

--- set_discard.py
+++ set_discard.py
@@ -8,3 +8,11 @@ my_set = set([12, 10, 13, 15, 8, 9])
 while my_set:
     my_set.discard(min(my_set))
     print(my_set)
+
+    """ Expected output: 
+{9, 10, 12, 13, 15}
+{10, 12, 13, 15}
+{12, 13, 15}
+{13, 15}
+{15}
+set()"""
