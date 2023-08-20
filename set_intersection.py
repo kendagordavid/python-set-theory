4dba05c1881e6f90658d1e6f9f8ec683d2616c3f
kendagordavid <davidkendgor7@gmail.com>
2023-08-20 22:21:50 +0300

    updated intersection() method

---
+++ set_intersection.py
@@ -0,0 +1,12 @@
+'''The intersection() method returns a new set with elements that are common to all sets.'''
+
+
+set_a = {"drinks", "carbs", "fruits", "vegetables", "dairy"}
+set_b = {"proteins", "carbohydrates", "vitamins", "alchohol", "fruits"}
+
+print(set_a.intersection(set_b))
+
+'''Expected output:{'fruits'}'''
+
+
+#love python
