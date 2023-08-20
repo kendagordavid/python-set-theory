4698f4ac409df1472b129f09e0ff548ea9d33ac8
kendagordavid <davidkendgor7@gmail.com>
2023-08-20 22:37:41 +0300

    updeted remove() method

---
+++ set_remove.py
@@ -0,0 +1,11 @@
+set_methods = {"isdisjoint","union","discard","pop","roots"}
+'''The line below removes the given element in the set'''
+
+set_methods.remove("roots")
+'''Then print the given set'''
+
+print(set_methods)
+
+'''Expected output: {'isdisjoint', 'discard', 'union', 'pop'}'''
+#love python
+
