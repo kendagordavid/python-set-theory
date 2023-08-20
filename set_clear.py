b9846424af220862f4382e7251b4cae3386e96a5
kendagordavid <davidkendgor7@gmail.com>
2023-08-20 09:42:24 +0300

    updated clear() method

--- set_clear.py
+++ set_clear.py
@@ -1,1 +1,12 @@
-set_ = {}
+
+# Python program to demonstrate working of
+# dictionary clear()
+text = {1: "yellow", 2: "orange"}
+ 
+text.clear()
+print('text =', text)
+
+""" Expected output: text = {} """
+
+
+#love python
